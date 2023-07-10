d2f28c8e8ab4e85db55e557e153b58b378eeb6e4
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-07-10 19:39:48 -0300

    section icon

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Hero.css';
+import { BsPinterest } from 'react-icons/bs';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="iconWapper"><BsPinterest className="icon" /></div>
+    </div>
+  )
+}
+
+export default Hero;
