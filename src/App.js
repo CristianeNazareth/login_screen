304a4c0a57269bd9b79b30581efac39c2cd16595
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-07-13 19:09:57 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
 import React from 'react'
-import Header from './Components/Header/Header.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import ButtonSocial from './Components/ButtonSocial/ButtonSocial.jsx';
 import './styles/variables.css';
 import Login from './Components/Login/Login.jsx';
-import ButtonLogin from './Components/ButtonLogin/ButtonLogin';
 import { BiLogoFacebook } from "react-icons/bi";
 import { BiLogoTwitter } from "react-icons/bi";
 import { BiLogoGoogle } from "react-icons/bi";
@@ -16,12 +14,9 @@ const App = () => {
 
       <React.Fragment>
 
-        <Header></Header>
         <Hero />
 
         <Login />
-        <ButtonLogin />
-
 
         <footer>
           <ButtonSocial icon={<BiLogoFacebook />} />
