2ddb39497641e90913970b396c71be6a8b7ab692
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-07-13 19:09:35 -0300

    login field

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import './Login.css';
+import ButtonSocial from '../ButtonSocial/ButtonSocial';
 
 const Login = () => {
   return (
+
     <div className="box-login">
       <div className="login-z">
         <p>PD</p>
         <hr className="line" />
         <p>ID</p>
-      </div>
+        </div>
+
+        <div className="wrapper">
+          <button className="login-btn">LOGIN</button>
+          <div className="wrapper-text">
+            <p>SIGN IN</p>
+            <p>FORGET?</p>
+          </div>
+        </div>
+          <ButtonSocial></ButtonSocial>
+
     </div>
   )
 }
